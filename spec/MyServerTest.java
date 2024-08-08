47dc777c275827627640e9db96682043a069e10c
Alex Root-Roatch <arootroatch@gmail.com>
2024-08-07 22:07:16 -0500

    terminal arguments

--- spec/MyServerTest.java
+++ spec/MyServerTest.java
@@ -1,19 +1,20 @@
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyServerTest {
-  static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  static ByteArrayOutputStream outContent;
   static final PrintStream originalOut = System.out;
 
-  @BeforeAll
-  static void setup() {
+  @BeforeEach
+  void setup() {
+    outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
   }
 
@@ -23,7 +24,7 @@ public class MyServerTest {
     MyServer.main(null);
     assertTrue(outContent.toString().contains("MyServer"));
     assertTrue(outContent.toString().contains("Running on port: 80"));
-    assertTrue(outContent.toString().contains("Serving files from:"));
+    assertTrue(outContent.toString().contains("Serving files from: root"));
   }
 
   @Test
@@ -31,6 +32,49 @@ public class MyServerTest {
     String[] args = {"-p", "1234"};
     MyServer.main(args);
     assertTrue(outContent.toString().contains("Running on port: 1234"));
+  }
+
+  @Test
+  void specifyRootDir() throws IOException{
+    String[] args = {"-r", "testroot"};
+    MyServer.main(args);
+    assertTrue(outContent.toString().contains("Serving files from: testroot"));
+  }
+
+  @Test
+  void printHelp() throws IOException {
+    String[] args = {"-h"};
+    MyServer.main(args);
+    assertTrue(outContent.toString().contains("  -p     Specify the port.  Default is 80."));
+    assertTrue(outContent.toString()
+        .contains("  -r     Specify the root directory.  Default is the current working directory."));
+    assertTrue(outContent.toString().contains("  -h     Print this help message"));
+    assertTrue(outContent.toString().contains("  -x     Print the startup configuration without starting the server"));
+  }
+
+  @Test
+  void printHelpDoesNotStartServer() throws IOException {
+    String[] args = {"-h"};
+    MyServer.main(args);
+    assertFalse(outContent.toString().contains("MyServer"));
+    assertFalse(outContent.toString().contains("Running on port: 80"));
+    assertFalse(outContent.toString().contains("Serving files from: root"));
+  }
+
+  @Test
+  void printConfig() throws IOException {
+    String[] args = {"-x"};
+    MyServer.main(args);
+    assertEquals("MyServer\n" + "Running on port: 80\n" + "Serving files from: root\n",
+        outContent.toString());
+  }
+
+  @Test
+  void printConfigWithDirAndPort() throws IOException {
+    String[] args = {"-r", "testroot", "-p", "1234", "-x"};
+    MyServer.main(args);
+    assertEquals("MyServer\n" + "Running on port: 1234\n" + "Serving files from: testroot\n",
+        outContent.toString());
   }
 
   @AfterAll
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,41 +1,65 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class MyServer {
   public static void main(String[] args) throws IOException {
-    String port;
-    setPort(args);
+    String port = setPort(args);
+    String rootDir = setRootDir(args);
 
-    try (ServerSocket serverSocket = new ServerSocket(80)){
-      System.out.println("MyServer");
-      System.out.println("Running on port: 80");
-      System.out.println("Serving files from:");
+    if (contains(args, "-h") > -1){
+      printHelp();
+      return;
+    }
+
+    if (contains(args, "-x") > -1){
+      printConfig(port, rootDir);
+      return;
+    }
+
+    try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port))) {
+        printConfig(port, rootDir);
 //      while (true){
 //        try (Socket client = serverSocket.accept()){
 //
 //        }
 //      }
-    } catch (IOException e){
+    } catch (IOException e) {
       System.err.println("Error handling client request: " + e.getMessage());
     }
   }
 
-  private static void setPort(String[] args, String port){
-    boolean specifiesPort = false;
-    int indexOfArg;
+  private static String setPort(String[] args) {
+    int indexOfArg = contains(args, "-p");
+    if (indexOfArg > -1) return args[indexOfArg + 1];
+    else return "80";
+  }
 
-    for (int i = 0; i < args.length; i++){
-      if (Objects.equals(args[i], "-p")){
-        specifiesPort = true;
-        indexOfArg = i;
-      }
-    }
+  private static String setRootDir(String[] args) {
+    int indexOfArg = contains(args, "-r");
+    if (indexOfArg > -1) return args[indexOfArg + 1];
+    else return "root";
+  }
 
-    if (specifiesPort){
-      port = args[indexOfArg + 1];
+  private static void printHelp() {
+    System.out.println("  -p     Specify the port.  Default is 80.");
+    System.out.println("  -r     Specify the root directory.  Default is the current working directory.");
+    System.out.println("  -h     Print this help message");
+    System.out.println("  -x     Print the startup configuration without starting the server");
+  }
+
+  private static int contains(String[] args, String s) {
+    if (args == null) return -1;
+
+    for (int i = 0; i < args.length; i++) {
+      if (Objects.equals(args[i], s)) return i;
     }
+    return -1;
+  }
+
+  private static void printConfig(String port, String rootDir){
+    System.out.println("MyServer");
+    System.out.println("Running on port: " + port);
+    System.out.println("Serving files from: " + rootDir);
   }
 }
